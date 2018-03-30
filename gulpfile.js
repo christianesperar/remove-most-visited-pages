b7a472488ccb3471accaf140b0d80a29b1cd9261
christianesperar <christianesperar@gmail.com>
2018-03-31 00:46:01 +0800

    Update gitignore and gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,35 @@ const babel = require('gulp-babel');
 const htmlmin = require('gulp-htmlmin');
 const jsonminify = require('gulp-jsonminify');
 
-const DEST = 'dist/';
+const dest = 'dist/';
+const src = 'src/';
 
 function build() {
-  gulp.src('src/*.js')
+  gulp.src(`${src}*.js`)
     .pipe(babel({
       presets: ['es2015'],
     }))
     .pipe(uglify())
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(dest));
 
-  gulp.src('src/*.html')
+  gulp.src(`${src}*.html`)
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(dest));
 
-  gulp.src('src/*.json')
+  gulp.src(`${src}*.json`)
     .pipe(jsonminify())
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(dest));
 
-  gulp.src('src/icons/*.png')
-    .pipe(gulp.dest(`${DEST}icons/`));
+  gulp.src(`${src}icons*.png`)
+    .pipe(gulp.dest(`${dest}icons/`));
 }
 
 function watch() {
   gulp.watch([
-    'src/*.js',
-    'src/*.html',
-    'src/*.json',
-    'src/icons/*.png',
+    `${src}*.js`,
+    `${src}*.html`,
+    `${src}*.json`,
+    `${src}icons/*.png`,
   ], [
     'build',
   ]);
